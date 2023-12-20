0be7b8a7173256e3a0e14fc81acf6621e6880983
estoledo <estoledo1@utpl.edu.ec>
2023-12-20 15:29:56 -0500

    arreglos

---
+++ Ejemplosc3/src/paquete05/ejemplo01.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete05;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class ejemplo01 {
+    public static void main(String[] args) {
+        int[] arreglo = new int [2];
+    }
+}
